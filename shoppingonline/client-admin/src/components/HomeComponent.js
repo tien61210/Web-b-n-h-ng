792e6cb0ce979a7c0f80694ea4d2c2914e4cef08
Nguyen Ngoc Tien <tien.207ct69361@vanlanguni.com>
2024-03-09 17:51:36 +0700

    Commit message

---
+++ shoppingonline/client-admin/src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">Tiệm Giày Cũ Sài Gòn</h2>
+        <img src="https://cdn.tgdd.vn/Files/2021/12/23/1406245/6-shop-giay-2hand-gia-re-va-uy-tin-nhat-sai-gon-202112230409393092.jpg" width="1000px" height="800px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ shoppingonline/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster01.8xxyisb.mongodb.net',
+  DB_USER: 'luongvuhong',
+  DB_PASS: '15122002',
+  DB_DATABASE: 'shoppingOnline',
+  JWT_SECRET: '4431',
+  JWT_EXPIRES: '31565890000000', // in milliseconds
+  EMAIL_USER: 'luongvuhong2202@gmail.com', // gmail service
+  EMAIL_PASS: 'eltuloolnrbvwzhy'
+};
+module.exports = MyConstants;
